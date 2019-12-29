33382051f6e177929be30314622c2dff89a1e38f
Varsha Panchal <varsha3051@gmail.com>
2019-12-28 19:12:20 -0500

    Install & Add Axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -81,7 +82,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        //alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Varsha Panchal',
+                address: {
+                    street: '44 street 1',
+                    zipcode: '42515',
+                    country: 'Indore'
+                },
+                email: 'varsha3051@gmail.com'
+            },
+            diliverMethod: 'fastest'
+        }
+        axios.post('/orders.json',order)
+          .then(response => console.log(response))
+          .catch(error => console.log(error));
     }
 
     render () {
